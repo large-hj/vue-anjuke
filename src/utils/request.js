10e9a9eb51f015ed9bb9c1a491ee39192926c328
chj <1679864294@qq.com>
2019-11-11 20:30:30 +0800

    初始化

---
+++ src/utils/request.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const server =axios.create({
+    timeout:5000,
+    withCredentials:true
+
+})
+
+// 请求拦截器
+server.interceptors.request.use((config)=>{
+    if(config.method=="get"){
+
+        return config.params={...config.data};
+    }
+    // 请求头信息
+    // config.headers["content-type"]="applicetion/json"
+},(err)=>{
+    return Promise.reject(err);
+})
+
+// 响应拦截
+server.interceptors.response.use((res)=>{
+    if(res.status==200){
+        return res.data;
+    }
+},(err)=>{
+    return Promise.reject(err);
+})
+export default server;
